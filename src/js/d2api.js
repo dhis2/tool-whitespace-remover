b9c1d4f3f12d769a6e61a44137e4e960ba122ed8
Olav <op.registrering@me.com>
2024-11-23 23:50:18 +0100

    Update d2api.js due to lint issue

--- src/js/d2api.js
+++ src/js/d2api.js
@@ -1,5 +1,5 @@
 const dhisDevConfig = DHIS_CONFIG; // eslint-disable-line
-const isDev = dhisDevConfig.hasOwnProperty("baseUrl") ? true : false;
+const isDev = "baseUrl" in dhisDevConfig;
 const baseUrl = isDev ? dhisDevConfig.baseUrl : "../../..";
 
 import $ from "jquery";
